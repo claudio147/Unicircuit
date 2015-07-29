e1cf7e96f084e22ff65b338da2a19335be5e3a6c
Luca Signoroni <lsgrafik@gmail.com>
2015-07-29 21:23:06 +0200

    Security: real_escape added in database connect_errno added in database
    filter_input for requests added in registration.php

--- library/public/database.inc.php
+++ library/public/database.inc.php
@@ -13,13 +13,37 @@ function connectDB() {
     $user = 'user04websrv02' ;
     $password = 'cfADs12P' ;
     $database = 'user04websrv02';
-    
- $link = mysqli_connect($host, $user, $password, $database) or die ('Verbindung zur Datenbank nicht möglich.');
+ // Stellt die Datenbank verbindung her
+ $link = mysqli_connect($host, $user, $password, $database);
  
+ 
+ //Prüft die Datenbank Verbindung
+ if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+ // liefert die DB Verbindund zurück
  return $link;
 }
 
-function createArchitect($fn, $ln, $co, $zip, $ci, $cn, $pn, $mn, $em, $to, $p1, $da, $ti) {
+function createArchitect($link, $fn, $ln, $co, $zip, $ci, $cn, $pn, $mn, $em, $to, $p1, $da, $ti) {
+    
+    //Escapen der Variablen, Verhinderung von SQL Injections.
+    $fn = mysqli_real_escape_string($link, $fn);
+    $ln = mysqli_real_escape_string($link, $ln);
+    $co = mysqli_real_escape_string($link, $co);
+    $zip = mysqli_real_escape_string($link, $zip);
+    $ci = mysqli_real_escape_string($link, $ci);
+    $cn = mysqli_real_escape_string($link, $cn);
+    $pn = mysqli_real_escape_string($link, $pn);
+    $mn = mysqli_real_escape_string($link, $mn);
+    $em = mysqli_real_escape_string($link, $em);
+    $to = mysqli_real_escape_string($link, $to);
+    $p1 = mysqli_real_escape_string($link, $p1);
+    $da = mysqli_real_escape_string($link, $da);
+    $ti = mysqli_real_escape_string($link, $ti);
+    
+    //Datenbank INSERT, fixe FK_IdUserType, hier handelt es sich immer um Architekt.
     $sql =  "INSERT INTO User (Firstname, Lastname, Company, ZIP, City, Country, 
               PhoneNumber, MobileNumber, Email, RegCode, Password, LastLoginDate, LastLoginTime, Fk_IdUserType) 
               VALUES('$fn', '$ln','$co',$zip,'$ci','$cn','$pn','$mn', '$em','$to', '$p1', '$da', '$ti', 2)";
@@ -27,12 +51,5 @@ function createArchitect($fn, $ln, $co, $zip, $ci, $cn, $pn, $mn, $em, $to, $p1,
     return $sql;
 }
 
-function createArchitect2($fn, $ln, $co, $zip, $ci, $cn, $pn, $mn, $em, $da, $ti, $p1, $to) {
-    $sql = "INSERT INTO User (Firstname, Lastname, Company, ZIP, City, Country, 
-              PhoneNumber, MobileNumber,  Email, LastLoginDate, LastLoginTime, Password, RegCode, Fk_IdUserType) 
-              VALUES('$fn', '$ln','$co',$zip,'$ci','$cn','$pn','$mn' '$em', '$da', '$ti', md5($p1), '$to', 2)";
-
-    return $sql;
-}
 
 
--- platform/public/php/registration.php
+++ platform/public/php/registration.php
@@ -20,22 +20,25 @@ function checkMailFormat($email) {
     
 }
 
-//Fehler Abfragen
-// Wurde Submit-Button angeklickt?
+/*
+ * Überprüfung ob Submit schon geklickt wurde.
+ * Fehler Abfrage
+ * Security: Bereinigung von möglichen falsch Eingaben.
+ */
 if (isset($_POST['submit'])) {
-  $fn = trim($_POST['firstname']);
-  $ln = trim($_POST['lastname']);
-  $co = trim($_POST['company']);
-  $a1 = trim($_POST['adressline1']);
-  $a2 = trim($_POST['adressline2']);
-  $zip = trim($_POST['zip']);
-  $ci = trim($_POST['city']);
-  $cn = trim($_POST['country']);
-  $pn = trim($_POST['PhoneNumber']);
-  $mn = trim($_POST['MobileNumber']);
-  $em = trim($_POST['email']);
-  $p1 = trim($_POST['password1']);
-  $p2 = trim($_POST['password2']);
+  $fn = filter_input(INPUT_POST, 'firstname', FILTER_SANITIZE_STRING);
+  $ln = filter_input(INPUT_POST, 'lastname', FILTER_SANITIZE_STRING);
+  $co = filter_input(INPUT_POST, 'company', FILTER_SANITIZE_STRING);
+  $a1 = filter_input(INPUT_POST, 'adressline1', FILTER_SANITIZE_STRING);
+  $a2 = filter_input(INPUT_POST, 'adressline2', FILTER_SANITIZE_STRING);
+  $zip = filter_input(INPUT_POST, 'zip', FILTER_SANITIZE_NUMBER_INT);
+  $ci = filter_input(INPUT_POST, 'city', FILTER_SANITIZE_STRING);
+  $cn = filter_input(INPUT_POST, 'country', FILTER_SANITIZE_STRING);
+  $pn = filter_input(INPUT_POST, 'PhoneNumber', FILTER_SANITIZE_STRING);
+  $mn = filter_input(INPUT_POST, 'MobileNumber', FILTER_SANITIZE_STRING);
+  $em = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+  $p1 = filter_input(INPUT_POST, 'password1', FILTER_SANITIZE_STRING);
+  $p2 = filter_input(INPUT_POST, 'password2', FILTER_SANITIZE_STRING);
 
   // Fehler im Eingabefeld?
   if (empty($fn) || strlen($fn) < 2) {
@@ -139,7 +142,7 @@ if (isset($_POST['submit'])) {
       // Füge alle Daten zusammen und genereriere einen HASH-Wert
       $to = sha1($fn . $ln . $em . $da . $ti . $ra . $sa);
       // SQL-Statement zusammensetzen, um Datensatz in DB zu speichern
-      $sql = createArchitect($fn, $ln, $co, $zip, $ci, $cn, $pn, $mn, $em, $to, $p1, $da, $ti);
+      $sql = createArchitect($link, $fn, $ln, $co, $zip, $ci, $cn, $pn, $mn, $em, $to, $p1, $da, $ti);
       // Anfrage an Datenbank senden
      
      
